607802db146137a72cb08b612231019d5e39598f
sandeep-negi-03 <sandeepnegi03@gmail.com>
2019-12-12 14:51:12 +0530

    adding currencyexchangeservice and currencyconversionservice

---
+++ Spring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		
+		return new CurrencyConversion(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+	}
+
+}
