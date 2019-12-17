9ab5c80679b743316918e03b7e0772762ca27262
Sandeep Negi <sannegi6@publicisgroupe.net>
2019-12-17 18:18:43 +0530

    changes for zuul, rabbitmq, zipkin server

--- Spring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ Spring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
@@ -39,6 +43,8 @@ public class CurrencyConversionController {
 			@PathVariable BigDecimal quantity) {
 
 		CurrencyConversion response = proxy.retrieveExchangeValue(from, to);
+		
+		logger.info("response : {}", response);
 
 		return new CurrencyConversion(1L, from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
--- Spring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ Spring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private ExchangeValueRepository exchangeValueRepository;
@@ -22,6 +26,9 @@ public class CurrencyExchangeController {
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("exchangeValue : {}", exchangeValue);
+		
 		return exchangeValue;
 	}
 
