07763eaafb0b970f8b6e0748374a329f33595e4e
Sandeep Negi <sannegi6@publicisgroupe.net>
2019-12-18 17:16:44 +0530

    changes for spring clould bus

--- Spring/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ Spring/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,15 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 	
 	@Autowired
 	private	Configuration configuration;
 
+	
 	@GetMapping("/limits-service")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfigurations")
+	public LimitConfiguration retrieveConfigurations() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfigurations() {
+		return new LimitConfiguration(9999, 9);
 	}
 	
 }
