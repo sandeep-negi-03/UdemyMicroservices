58f8a5d00dc0bcf6f389b8904b898de18284f233
Sandeep Negi <sannegi6@publicisgroupe.net>
2019-12-24 17:01:46 +0530

    added jpa in 10 steps

---
+++ Spring/jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserDaoServiceCommandlineRunner.java
@@ -0,0 +1,27 @@
+package com.in28minutes.learning.jpa.jpain10steps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+import com.in28minutes.learning.jpa.jpain10steps.service.UserDAOService;
+
+@Component
+public class UserDaoServiceCommandlineRunner implements CommandLineRunner{
+
+	private static final Logger logger = LoggerFactory.getLogger(UserDaoServiceCommandlineRunner.class);
+	
+	@Autowired
+	private UserDAOService userDAOService; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("Jack", "Admin");
+		userDAOService.intsert(user);
+		logger.info("New User is created : " + user);
+	}
+
+}
